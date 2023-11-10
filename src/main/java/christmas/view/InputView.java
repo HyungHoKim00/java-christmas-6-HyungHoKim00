50eeca30614292aabf2e36d01010c2d8a9339063
HyungHoKim <hkim1109@naver.com>
2023-11-10 22:23:42 +0900

    feat(InputView): 방문 날짜 입력
    방문 날짜를 입력하면 날짜를 숫자로 반환하는 기능 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int inputDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+    
+}
