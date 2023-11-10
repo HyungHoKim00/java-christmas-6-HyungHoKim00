a4fb0b15cd1c29f39601e13bc97b0b69e3ef7114
HyungHoKim <hkim1109@naver.com>
2023-11-10 21:57:24 +0900

    feat(Badge): 배지 부여 선정
    배지 객체 생성 및 12월 이벤트 배지 부여 선정 기능 추가

---
+++ src/main/java/christmas/enums/Badge.java
@@ -0,0 +1,34 @@
+package christmas.enums;
+
+import java.util.Arrays;
+
+public enum Badge {
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("스타", 5_000),
+    INVALID_BADGE("", 0);
+
+    private final String inKorean;
+    private final int leastCondition;
+
+    Badge(String inKorean, int leastCondition) {
+        this.inKorean = inKorean;
+        this.leastCondition = leastCondition;
+    }
+
+    public String inKorean() {
+        return inKorean;
+    }
+
+    public int leastCondition() {
+        return leastCondition;
+    }
+
+    public String determineBadge(int totalDiscountPrice) {
+        return Arrays.stream(values())
+                .filter(badge -> totalDiscountPrice >= badge.leastCondition())
+                .findFirst()
+                .orElse(INVALID_BADGE)
+                .inKorean();
+    }
+}
