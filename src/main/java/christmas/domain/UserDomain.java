9171e627aff67bc9a9ab6362bce229101d314687
HyungHoKim <hkim1109@naver.com>
2023-11-10 21:34:34 +0900

    feat(UserDomain): 특별 할인
    특별 할인 금액을 반환하는 기능 추가

--- src/main/java/christmas/domain/UserDomain.java
+++ src/main/java/christmas/domain/UserDomain.java
@@ -7,46 +7,58 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class UserDomain {
-    private static final List<Integer> WEEKEND = List.of(1,2,8,9,15,16,22,23,29,30);
+    private static final List<Integer> WEEKEND = List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private static final List<Integer> STAR_DATE = List.of(3, 10, 17, 24, 25, 31);
+    private static final String WEEKDAY_DISCOUNT_TYPE = "디저트";
+    private static final String WEEKEND_DISCOUNT_TYPE = "메인";
     private static final int CHRISTMAS_DATE = 25;
-    private static final int NO_SALE = 0;
-    private static final int D_DAY_BASIC_SALE = 900;
-    private static final int D_DAY_BONUS_SALE = 100;
+    private static final int NO_DISCOUNT = 0;
+    private static final int D_DAY_BASIC_DISCOUNT = 900;
+    private static final int D_DAY_BONUS_DISCOUNT = 100;
+    private static final int YEAR_DISCOUNT = 2023;
+    private static final int SPECIAL_DISCOUNT = 1000;
 
-    public static int calculateTotalOrderPrice(Map<Menu,Integer> order){
+    public static int calculateTotalOrderPrice(Map<Menu, Integer> order) {
         AtomicInteger totalOrderPrice = new AtomicInteger();
         order.keySet()
                 .forEach(key -> totalOrderPrice.addAndGet(key.price() * order.get(key)));
         return totalOrderPrice.get();
     }
 
-    public static int getDDaySale(int date){
-        if(date<=CHRISTMAS_DATE){
-            return D_DAY_BASIC_SALE+date*D_DAY_BONUS_SALE;
+    public static int getDDayDiscount(int date) {
+        if (date <= CHRISTMAS_DATE) {
+            return D_DAY_BASIC_DISCOUNT + date * D_DAY_BONUS_DISCOUNT;
         }
-        return NO_SALE;
+        return NO_DISCOUNT;
     }
 
-    public static int getWeekDaySale(int date, Map<Menu,Integer> order){
-        if(!WEEKEND.contains(date)){
-            AtomicInteger saleAmount = new AtomicInteger();
+    public static int getWeekdayDiscount(int date, Map<Menu, Integer> order) {
+        if (!WEEKEND.contains(date)) {
+            AtomicInteger discountAmount = new AtomicInteger();
             order.keySet().stream()
-                    .filter(key -> Objects.equals(key.type(), "디저트"))
-                    .forEach(key -> saleAmount.addAndGet(2023));
-            return saleAmount.get();
+                    .filter(key -> Objects.equals(key.type(), WEEKDAY_DISCOUNT_TYPE))
+                    .forEach(key -> discountAmount.addAndGet(YEAR_DISCOUNT));
+            return discountAmount.get();
         }
-        return NO_SALE;
+        return NO_DISCOUNT;
     }
 
-    public static int getWeekendSale(int date, Map<Menu,Integer> order){
-        if(WEEKEND.contains(date)){
-            AtomicInteger saleAmount = new AtomicInteger();
+    public static int getWeekendDiscount(int date, Map<Menu, Integer> order) {
+        if (WEEKEND.contains(date)) {
+            AtomicInteger discountAmount = new AtomicInteger();
             order.keySet().stream()
-                    .filter(key -> Objects.equals(key.type(), "메인"))
-                    .forEach(key -> saleAmount.addAndGet(2023));
-            return saleAmount.get();
+                    .filter(key -> Objects.equals(key.type(), WEEKEND_DISCOUNT_TYPE))
+                    .forEach(key -> discountAmount.addAndGet(SPECIAL_DISCOUNT));
+            return discountAmount.get();
         }
-        return NO_SALE;
+        return NO_DISCOUNT;
+    }
+
+    public static int getSpecialSale(int date) {
+        if (STAR_DATE.contains(date)) {
+            return 1000;
+        }
+        return 0;
     }
 
 }
