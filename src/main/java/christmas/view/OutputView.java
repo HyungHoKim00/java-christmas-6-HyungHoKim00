9405fefa3e550f22aa3c969aaf8dd011072f2177
HyungHoKim <hkim1109@naver.com>
2023-11-11 10:51:18 +0900

    feat(OutputView): 주문 메뉴 출력
    주문 메뉴를 형식에 맞게 출력하는 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,35 @@
+package christmas.view;
+
+import christmas.enums.Menu;
+import java.util.Map;
+
+public class OutputView {
+    public void printTotal(int date) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 혜택 미리 보기!" + "%n", date);
+        System.out.println();
+    }
+
+    public void printMenu(Map<Menu, Integer> order) {
+        System.out.println("<주문 메뉴>");
+        order.keySet()
+                .forEach(menu -> {
+                    String menuName = menu.inKorean();
+                    String menuAmount = order.get(menu).toString();
+                    System.out.println(menuName + " " + menuAmount + "개");
+                });
+    }
+
+    
+    public void printDiscountDetails(Map<String, Integer> discountDetails) {
+    }
+
+    public void printTotalDiscount(int discountDetails) {
+    }
+
+    public void printEstimatedPrice(int i, int totalDiscount) {
+
+    }
+
+    public void printEventBadge(int totalDiscount) {
+    }
+}
