8eb1f791b381ff9bd975edba20c33ea6c7d9b704
HyungHoKim <hkim1109@naver.com>
2023-11-14 22:06:24 +0900

    refactor(Event): 불필요한 상수 제거
    INVALID_EVENT 제거

--- src/main/java/christmas/enums/Event.java
+++ src/main/java/christmas/enums/Event.java
@@ -7,8 +7,7 @@ public enum Event {
     WEEKDAY_EVENT("평일 할인", 2_023),
     WEEKEND_EVENT("주말 할인", 2_023),
     SPECIAL_EVENT("특별 할인", 1_000),
-    GIFT_EVENT("증정 이벤트", GIFT_EVENT_GIFT.getPrice()),
-    NONE("", 0);
+    GIFT_EVENT("증정 이벤트", GIFT_EVENT_GIFT.getPrice());
 
     private final String name;
     private final int discount;
